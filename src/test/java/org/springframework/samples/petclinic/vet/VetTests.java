d5ad707481b640275f8e2a5a419b722cbc9d89e4
przemyslawbarcik <pbarcik89@gmail.com>
2024-11-18 16:09:21 +0100

    break test

--- src/test/java/org/springframework/samples/petclinic/vet/VetTests.java
+++ src/test/java/org/springframework/samples/petclinic/vet/VetTests.java
@@ -16,9 +16,6 @@
 package org.springframework.samples.petclinic.vet;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.util.SerializationUtils;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Dave Syer
@@ -27,14 +24,7 @@ class VetTests {
 
 	@Test
 	void testSerialization() {
-		Vet vet = new Vet();
-		vet.setFirstName("Zaphod");
-		vet.setLastName("Beeblebrox");
-		vet.setId(123);
-		Vet other = (Vet) SerializationUtils.deserialize(SerializationUtils.serialize(vet));
-		assertThat(other.getFirstName()).isEqualTo(vet.getFirstName());
-		assertThat(other.getLastName()).isEqualTo(vet.getLastName());
-		assertThat(other.getId()).isEqualTo(vet.getId());
+		System.out.println("Hello Test");
 	}
 
 }
